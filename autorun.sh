f53b36ef0d747a75ceeb7e430421d10f538b00d7
naveenland4 <32125555+naveenland4@users.noreply.github.com>
2023-09-16 15:46:24 +0530

    Update autorun.sh

--- autorun.sh
+++ autorun.sh
@@ -6,6 +6,6 @@ python3 -m pip install requests
 
 cd $(dirname $0)/scripts/
 
-python3 youtube_m3ugrabber.py > ../youtube.m3u
+python3 youtube_m3ugrabber.py > ../news_channels.m3u
 
 echo m3u grabbed
