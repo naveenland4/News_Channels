efec9ca8fb0f4749f570e63b2cad26071104c861
naveenland4 <32125555+naveenland4@users.noreply.github.com>
2024-08-20 15:58:03 +0530

    Update youtube_m3ugrabber.py

--- scripts/youtube_m3ugrabber.py
+++ scripts/youtube_m3ugrabber.py
@@ -34,13 +34,13 @@ def grab(url):
         response = requests.get(url).text
         if '.m3u8' not in response:
             if windows:
-                print('https://raw.githubusercontent.com/naveenland4/YouTube_to_m3u/main/assets/info.m3u8')
+                print('https://raw.githubusercontent.com/naveenland4/News_Channels/main/assets/info.m3u8')
                 return
             #os.system(f'wget {url} -O temp.txt')
             os.system(f'curl "{url}" > temp.txt')
             response = ''.join(open('temp.txt').readlines())
             if '.m3u8' not in response:
-                print('https://raw.githubusercontent.com/naveenland4/YouTube_to_m3u/main/assets/info.m3u8')
+                print('https://raw.githubusercontent.com/naveenland4/News_Channels/main/assets/info.m3u8')
                 return
     end = response.find('.m3u8') + 5
     tuner = 100
